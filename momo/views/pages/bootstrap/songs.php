2ef8b4ce854f3263c091eaee8c83929f8070a611
Hyerim Yeom <ringkle777@gmail.com>
2021-02-19 01:34:23 +0900

    #1551 동요 부분 현재까지 작업된 스토리보드 적용

--- momo/views/pages/bootstrap/songs.php
+++ momo/views/pages/bootstrap/songs.php
@@ -1,1 +1,165 @@
-songs page
+<!DOCTYPE html>
+<html lang="kor">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>동요 | 모심</title>
+
+<!-- Stylesheets -->
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+</head>
+
+<!-- page wrapper -->
+<body class="boxed_wrapper">
+
+
+    <!-- .preloader -->
+    <div class="preloader"></div>
+    <!-- /.preloader -->
+
+
+    <!-- search-box-layout -->
+    <!-- <div class="wraper_flyout_search">
+        <div class="table">
+            <div class="table-cell">
+                <div class="flyout-search-layer"></div>
+                <div class="flyout-search-layer"></div>
+                <div class="flyout-search-layer"></div>
+                <div class="flyout-search-close">
+                    <span class="flyout-search-close-line"></span>
+                    <span class="flyout-search-close-line"></span>
+                </div>
+                <div class="flyout_search">
+                    <div class="flyout-search-title">
+                        <h4>Search</h4>
+                    </div>
+                    <div class="flyout-search-bar">
+                        <form role="search" method="get" action="#">
+                            <div class="form-row">
+                                <input type="search" placeholder="Type to search..." value="" name="s" required="">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- search-box-layout end -->
+
+    <!--Page Title-->
+    <section class="page-title centred" style="background-image: url(images/background/page-title.jpg);">
+        <div class="container">
+            <div class="content-box">
+                <h1>Our Classes</h1>
+                <ul class="bread-crumb clearfix">
+                    <li><a href="index.html">Home</a></li>
+                    <li>Classes</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!--End Page Title-->
+    
+    <!-- our-teachers -->
+    <section class="our-teachers sec-pad centred">
+        <div class="container">
+            <div class="sec-title">
+                <h5>Teachers</h5>
+                <h1>World Best Teacher<br />Will Teach</h1>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-1.png" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Kevin Spacey</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-2.png" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Catherine Jones</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-3.png" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">John Travolta</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- our-teachers end -->
+
+
+
+
+<!--Scroll to top-->
+<button class="scroll-top scroll-to-target" data-target="html">
+    <i class="fa fa-arrow-up"></i>
+</button>
+
+
+
+<!-- jequery plugins -->
+<!-- <script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/validation.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/parallax.min.js"></script> -->
+
+<!-- main-js -->
+<!-- <script src="js/script.js"></script> -->
+
+</body><!-- End of .page_wrapper -->
+</html>
