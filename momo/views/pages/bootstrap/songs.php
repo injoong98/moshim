9d1721ae5c29ec17f2aa784d1a28809ec703273e
Hyerim Yeom <ringkle777@gmail.com>
2021-02-19 01:40:49 +0900

    #1551 동요 페이지 수정

--- momo/views/pages/bootstrap/songs.php
+++ momo/views/pages/bootstrap/songs.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="kor">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>동요 | 모심</title>
-
-<!-- Stylesheets -->
-<link href="css/style.css" rel="stylesheet">
-<link href="css/responsive.css" rel="stylesheet">
-<link rel="icon" href="images/favicon.ico" type="image/x-icon">
-
-</head>
+ 
 
 <!-- page wrapper -->
 <body class="boxed_wrapper">
@@ -157,6 +143,3 @@
         </div>
     </section>
     <!-- activities-section end -->
-
-</body><!-- End of .page_wrapper -->
-</html>
