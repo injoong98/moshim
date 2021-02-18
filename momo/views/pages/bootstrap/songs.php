ed47b11b7ce7e675afac2db6547864fb855453e4
Hyerim Yeom <ringkle777@gmail.com>
2021-02-19 01:35:40 +0900

    #1551 동요 화면 수정

--- momo/views/pages/bootstrap/songs.php
+++ momo/views/pages/bootstrap/songs.php
@@ -17,41 +17,6 @@
 <!-- page wrapper -->
 <body class="boxed_wrapper">
 
-
-    <!-- .preloader -->
-    <div class="preloader"></div>
-    <!-- /.preloader -->
-
-
-    <!-- search-box-layout -->
-    <!-- <div class="wraper_flyout_search">
-        <div class="table">
-            <div class="table-cell">
-                <div class="flyout-search-layer"></div>
-                <div class="flyout-search-layer"></div>
-                <div class="flyout-search-layer"></div>
-                <div class="flyout-search-close">
-                    <span class="flyout-search-close-line"></span>
-                    <span class="flyout-search-close-line"></span>
-                </div>
-                <div class="flyout_search">
-                    <div class="flyout-search-title">
-                        <h4>Search</h4>
-                    </div>
-                    <div class="flyout-search-bar">
-                        <form role="search" method="get" action="#">
-                            <div class="form-row">
-                                <input type="search" placeholder="Type to search..." value="" name="s" required="">
-                                <button type="submit"><i class="fa fa-search"></i></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!-- search-box-layout end -->
-
     <!--Page Title-->
     <section class="page-title centred" style="background-image: url(images/background/page-title.jpg);">
         <div class="container">
