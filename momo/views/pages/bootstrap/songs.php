d0d1862d1abe60f069ee6af7fdea9da908338b89
Hyerim Yeom <ringkle777@gmail.com>
2021-02-19 18:28:41 +0900

    #1551 동요 부분 피드백 반영

--- momo/views/pages/bootstrap/songs.php
+++ momo/views/pages/bootstrap/songs.php
@@ -1,8 +1,5 @@
  
 
-<!-- page wrapper -->
-<body class="boxed_wrapper">
-
     <!--Page Title-->
     <section class="page-title centred" style="background-image: url(images/background/page-title.jpg);">
         <div class="container">
@@ -18,7 +15,7 @@
     <!--End Page Title-->
     
     <!-- our-teachers -->
-    <section class="our-teachers sec-pad centred">
+    <section class="our-teachers teachers-page-section sec-pad centred">
         <div class="container">
             <div class="sec-title">
                 <h5>Teachers</h5>
@@ -29,7 +26,7 @@
                     <div class="teachers-block-one wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
                         <div class="inner-box">
                             <div class="image-holder">
-                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-1.png" alt=""></a></figure>
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
                             </div>
                             <div class="lower-content">
                                 <h3><a href="#">Kevin Spacey</a></h3>
@@ -48,7 +45,7 @@
                     <div class="teachers-block-one wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
                         <div class="inner-box">
                             <div class="image-holder">
-                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-2.png" alt=""></a></figure>
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
                             </div>
                             <div class="lower-content">
                                 <h3><a href="#">Catherine Jones</a></h3>
@@ -67,10 +64,183 @@
                     <div class="teachers-block-one wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
                         <div class="inner-box">
                             <div class="image-holder">
-                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-3.png" alt=""></a></figure>
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
                             </div>
                             <div class="lower-content">
                                 <h3><a href="#">John Travolta</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Christine Rose</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Fernando Vicente</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Susan Hardson</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Kevin Spacey</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Catherine Jones</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">John Travolta</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Christine Rose</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Fernando Vicente</a></h3>
+                                <span class="designation">Teacher</span>
+                                <ul class="social-list">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-12 block-column">
+                    <div class="teachers-block-one wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <div class="inner-box">
+                            <div class="image-holder">
+                                <figure class="image-box"><a href="#"><img src="https://via.placeholder.com/280" alt=""></a></figure>
+                            </div>
+                            <div class="lower-content">
+                                <h3><a href="#">Susan Hardson</a></h3>
                                 <span class="designation">Teacher</span>
                                 <ul class="social-list">
                                     <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -143,3 +313,48 @@
         </div>
     </section>
     <!-- activities-section end -->
+
+    <!-- fact-counter -->
+    <section class="fact-counter">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-12 counter-column">
+                    <div class="counter-block wow slideInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-two-users"></i></div>
+                        <div class="count-outer count-box">
+                            <span class="count-text" data-speed="1500" data-stop="200">0</span>
+                        </div>
+                        <div class="text">Teacher & Staffs</div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 counter-column">
+                    <div class="counter-block wow slideInUp" data-wow-delay="100ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-calendar"></i></div>
+                        <div class="count-outer count-box">
+                            <span class="count-text" data-speed="1500" data-stop="125">0</span>
+                        </div>
+                        <div class="text">Total Sessions</div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 counter-column">
+                    <div class="counter-block wow slideInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-boy"></i></div>
+                        <div class="count-outer count-box">
+                            <span class="count-text" data-speed="1500" data-stop="750">0</span>
+                        </div>
+                        <div class="text">Total Students</div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 counter-column">
+                    <div class="counter-block wow slideInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-flask"></i></div>
+                        <div class="count-outer count-box">
+                            <span class="count-text" data-speed="1500" data-stop="200">0</span>
+                        </div>
+                        <div class="text">Labs Project</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- fact-counter end -->
