cf5fb310af936b10666f96d8183148a2f78f11cc
seikunlee <saykunlee@gmail.com>
2021-02-19 13:23:59 +0900

    #1548: 비전

--- momo/views/pages/bootstrap/vision.php
+++ momo/views/pages/bootstrap/vision.php
@@ -1,1 +1,139 @@
-vision page 
+ <!-- main-slider -->
+ <section class="main-slider style-three">
+     <div class="main-slider-carousel owl-carousel owl-theme nav-style-two">
+         <div class="slide" style="background-image:url(<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/main-slider/slider-1.png)">
+             <div class="container">
+                 <div class="content-box">
+                     <h3>Best Choise</h3>
+                     <h1>Make Your Children's Life Special</h1>
+                     <!--        <div class="btn-box"><a href="contact.html" class="theme-btn">Contact us</a></div> -->
+                 </div>
+             </div>
+         </div>
+
+     </div>
+ </section>
+ <!-- main-slider end -->
+
+
+ <!-- service-section -->
+ <section class="service-style-two">
+     <div class="container">
+         <div class="inner-content">
+             <div class="row">
+                 <div class="col-lg-4 col-md-6 col-sm-12 service-column">
+                     <div class="service-block-one">
+                         <div class="inner-box">
+                             <div class="icon-box"><i class="flaticon-bus-1"></i></div>
+                             <h3><a href="#">Bus Service</a></h3>
+                             <div class="text">Cupidatat non proident sunt culpa qui officia deserunt mollit anim.</div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-lg-4 col-md-6 col-sm-12 service-column">
+                     <div class="service-block-one">
+                         <div class="inner-box">
+                             <div class="icon-box"><i class="flaticon-running"></i></div>
+                             <h3><a href="#">Sports Training</a></h3>
+                             <div class="text">Cupidatat non proident sunt culpa qui officia deserunt mollit anim.</div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-lg-4 col-md-6 col-sm-12 service-column">
+                     <div class="service-block-one">
+                         <div class="inner-box">
+                             <div class="icon-box"><i class="flaticon-drum"></i></div>
+                             <h3><a href="#">Music Training</a></h3>
+                             <div class="text">Cupidatat non proident sunt culpa qui officia deserunt mollit anim.</div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+ <!-- service-section end -->
+ <!--Page Title-->
+ <section class="page-title centred" style="background-image: url(<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/background/page-title.jpg);">
+     <div class="container">
+         <div class="content-box">
+             <h1>Youtube</h1>
+             <!-- <ul class="bread-crumb clearfix">
+                    <li><a href="index.html">Home</a></li>
+                    <li>About</li>
+                </ul> -->
+         </div>
+     </div>
+ </section>
+ <!--End Page Title-->
+ <!-- about-section -->
+ <section class="about-section style-two">
+     <div class="anim-icon">
+         <div class="icon icon-1 float-bob-x"></div>
+         <div class="icon icon-3"></div>
+     </div>
+     <div class="container">
+         <div class="row">
+             <div class="col-lg-6 col-md-12 col-sm-12 content-column">
+                 <div class="content-box">
+                     <div class="sec-title style-two">
+                         <h5>About Us</h5>
+                         <h1>Welcome to Kidko Kindergarten</h1>
+                     </div>
+                     <div class="bold-text">Cupidatat non proident sunt culpa qui officia deserunt mollit anim idest laborum</div>
+                     <div class="text">
+                         <p>Cupidatat non proident sunt culpa qui officia deserunt mollit anim idest laborum sed ut perspiciatis unde omnis iste natuserror sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.</p>
+                     </div>
+                     <div class="btn-box"><a href="contact.html" class="theme-btn">Contact us</a></div>
+                 </div>
+             </div>
+             <div class="col-lg-6 col-md-12 col-sm-12 image-column">
+                 <div class="image-box wow fadeInRight" data-wow-delay="0ms" data-wow-duration="1500ms">
+                     <figure class="image image-1"><img src="<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/resource/about-6.jpg" alt=""></figure>
+                     <figure class="image image-2"><img src="<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/resource/about-5.jpg" alt=""></figure>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+ <!-- about-section end -->
+
+ <!-- offer-section -->
+ <section class="offer-section sec-pad centred">
+        <div class="anim-icon">
+            <div class="icon icon-1"></div>
+        </div>
+        <div class="container">
+            <div class="sec-title">
+                <h5>Benefits</h5>
+                <h1>What We Offer</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
+                    <div class="single-offer-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-bus-1"></i></div>
+                        <h3><a href="#">Bus Service</a></h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
+                    <div class="single-offer-block wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-running"></i></div>
+                        <h3><a href="#">Sports Training</a></h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
+                    <div class="single-offer-block wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-drum"></i></div>
+                        <h3><a href="#">Music Training</a></h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
+                    <div class="single-offer-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <div class="icon-box"><i class="flaticon-professor"></i></div>
+                        <h3><a href="#">Best Teachers</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- offer-section end -->
