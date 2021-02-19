68412189f91e3d7fc8c70afc8d2df3a237b11fe1
seikunlee <saykunlee@gmail.com>
2021-02-19 13:42:59 +0900

    #1549: 유튜브

--- momo/views/pages/bootstrap/youtube.php
+++ momo/views/pages/bootstrap/youtube.php
@@ -1,1 +1,136 @@
-youtube page
+  <!-- main-slider -->
+  <section class="main-slider style-two">
+        <div class="main-slider-carousel owl-carousel owl-theme nav-style-two">
+            <div class="slide" style="background-image:url(<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/main-slider/slider-4.jpg)">
+                <div class="container">
+                    <div class="content-box">
+                        <h3>Best Choise</h3>
+                        <h1>Make Your Children's Life Special</h1>
+                        <div class="btn-box"><a href="contact.html" class="theme-btn">Contact us</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="slide" style="background-image:url(<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/main-slider/slider-4.jpg)">
+                <div class="container">
+                    <div class="content-box">
+                        <h3>Best Choise</h3>
+                        <h1>Make Your Children's Life Special</h1>
+                        <div class="btn-box"><a href="contact.html" class="theme-btn">Contact us</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="slide" style="background-image:url(<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/main-slider/slider-4.jpg)">
+                <div class="container">
+                    <div class="content-box">
+                        <h3>Best Choise</h3>
+                        <h1>Make Your Children's Life Special</h1>
+                        <div class="btn-box"><a href="contact.html" class="theme-btn">Contact us</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- main-slider end -->
+
+    <!-- feature-section -->
+    <section class="feature-section">
+    <div class="anim-icon">
+        <div class="icon icon-1"></div>
+        <div class="icon icon-2"></div>
+    </div>
+    <div class="image-column" style="background-image: url(<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/background/feature-bg.jpg);"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 col-lg-12 col-md-12 offset-xl-7 inner-column">
+                <div class="inner-content">
+                    <div class="sec-title style-two">
+                        <h5>Facilities</h5>
+                        <h1>Best Facilities<br />for Kids</h1>
+                    </div>
+                    <div class="inner-box">
+                        <div class="single-item wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="icon-box"><i class="flaticon-bus"></i></div>
+                            <h3><a href="#">Transportation</a></h3>
+                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed eiusmod tempor incididunt labore.</div>
+                        </div>
+                        <div class="single-item wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="icon-box"><i class="flaticon-console"></i></div>
+                            <h3><a href="#">Outdoor Games</a></h3>
+                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed eiusmod tempor incididunt labore.</div>
+                        </div>
+                        <div class="single-item wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="icon-box"><i class="flaticon-harvest"></i></div>
+                            <h3><a href="#">Nutritious Food</a></h3>
+                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed eiusmod tempor incididunt labore.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- feature-section end -->
+
+    <!-- classes-section -->
+    <section class="classes-section style-two sec-pad">
+        <div class="parallax-scene parallax-scene-1 parallax-icon">
+            <span data-depth="0.40" class="parallax-layer icon icon-1"></span>
+            <span data-depth="0.50" class="parallax-layer icon icon-2"></span>
+            <span data-depth="0.30" class="parallax-layer icon icon-3"></span>
+            <span data-depth="0.40" class="parallax-layer icon icon-4"></span>
+            <span data-depth="0.50" class="parallax-layer icon icon-5"></span>
+            <span data-depth="0.30" class="parallax-layer icon icon-6"></span>
+            <span data-depth="0.40" class="parallax-layer icon icon-7"></span>
+        </div>
+        <div class="container">
+            <div class="sec-title centred">
+                <h5>Classess</h5>
+                <h1>Education for Your<br />Children</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                    <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                        <figure class="image-box"><a href="class-details.html"><img src="<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/resource/class-1.jpg" alt=""></a></figure>
+                        <div class="lower-content">
+                            <div class="link-btn"><a href="class-details.html"><i class="flaticon-next"></i></a></div>
+                            <h3><a href="class-details.html">Music Lessons</a></h3>
+                            <div class="price">$480</div>
+                            <ul class="info-box">
+                                <li>Age: <span>2-4 Years</span></li>
+                                <li>Size: <span>12 Seats</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                    <div class="inner-block wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <figure class="image-box"><a href="class-details.html"><img src="<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/resource/class-2.jpg" alt=""></a></figure>
+                        <div class="lower-content">
+                            <div class="link-btn"><a href="class-details.html"><i class="flaticon-next"></i></a></div>
+                            <h3><a href="class-details.html">Paper Plates Art</a></h3>
+                            <div class="price">$580</div>
+                            <ul class="info-box">
+                                <li>Age: <span>2-4 Years</span></li>
+                                <li>Size: <span>12 Seats</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box"><a href="class-details.html"><img src="<?php echo trim(site_url(), '/'); ?>/momo/assets/moshim/images/resource/class-3.jpg" alt=""></a></figure>
+                        <div class="lower-content">
+                            <div class="link-btn"><a href="class-details.html"><i class="flaticon-next"></i></a></div>
+                            <h3><a href="class-details.html">Education Lessons</a></h3>
+                            <div class="price">$550</div>
+                            <ul class="info-box">
+                                <li>Age: <span>2-4 Years</span></li>
+                                <li>Size: <span>12 Seats</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- classes-section end -->
