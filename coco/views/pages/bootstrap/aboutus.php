7d00637f54632ad36d7256d740621f5ccc8b0ecf
Hyerim Yeom <ringkle777@gmail.com>
2021-02-21 13:20:00 +0900

    #1557 회사소개

--- coco/views/pages/bootstrap/aboutus.php
+++ coco/views/pages/bootstrap/aboutus.php
@@ -1,1 +1,206 @@
-aboutus
+  <!-- breadcrumb part -->
+  <section class="breadcrumb_part parallax_bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 ">
+                    <div class="breadcrumb_iner">
+                        <h2>회사소개</h2>
+                        <div class="breadcrumb_iner_link">
+                            <a href="index.html">Home</a>
+                            <span>|</span>
+                            <p>About us</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb_animation_4">
+            <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_5">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_7">
+            <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_10">
+            <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_12">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_13">
+            <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb part end -->
+
+    <!-- services part here -->
+     <section class="cta_section section_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-9 col-sm-10 wow fadeInUp" data-wow-delay=".3s">
+                    <div class="cta_section_wrapper">
+                        <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/cta_img.png" alt="#" class="img-fluid">
+                        <h2>Enrollment Is Now Going On</h2>
+                        <p>The amazing way to olor sit amet, consectetur adipisicing elit, sed do eiusmod incididunt ut
+                            labore
+                            inventore Theamaolor sit amet, consectetur. The amazing way to olor</p>
+                        <a class="pc-button elementor-button button-link cu_btn" href="contact.html">
+                            <div class="button-content-wrapper ">
+                                <span class="elementor-button-text">Contact Us</span>
+                                <svg class="pc-dashes inner-dashed-border animated-dashes">
+                                    <rect x="5px" y="5px" rx="22px" ry="22px" width="0" height="0"></rect>
+                                </svg>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cta_animation_1">
+            <div data-parallax='{"x": 2, "y": 120, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/cta_1.png" alt="#"></div>
+        </div>
+        <div class="cta_animation_2">
+            <div data-parallax='{"x": 10, "y": 100, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/event_7.png" alt="#">
+            </div>
+        </div>
+        <div class="cta_animation_3">
+            <div data-parallax='{"x": 30, "y": 110, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/cta_3.png" alt="#">
+            </div>
+        </div>
+        <div class="cta_animation_4">
+            <div data-parallax='{"x": 5, "y": 105, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_two_3.png" alt="#"></div>
+        </div>
+    </section>
+    <!-- services part end -->
+
+
+    <!-- about section part here -->
+     <section class="about_section section_padding s4_about_section">
+        <div class="container custom_container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <div class="about_section_content mt-5 mt-lg-0">
+                        <h5 class="kid_title wow fadeInLeft" data-wow-delay=".3s">Who We Are</h5>
+                        <h2 class="kid_title wow fadeInLeft" data-wow-delay=".4s"> <span class="title_overlay_effect">We Create New</span></h2>
+                        <p class="wow fadeInLeft" data-wow-delay=".5s">The amazing way to olor sit amet, consectetur 
+                        adipisicing elit, sed do eiusmod incididunt ut labore inventore The amazing way to olor sit 
+                        amet, consectetur. consectetur adipisicing elit, sed do eiusmod incididunt ut labore inventore.</p>
+                        <div class="list_content">
+                            <ul>
+                                <li class="wow fadeInLeft" data-wow-delay=".5s"><i class="far fa-check-circle"></i> Best
+                                    Learning School for Kids</li>
+                                <li class="wow fadeInLeft" data-wow-delay=".5s"><i class="far fa-check-circle"></i>
+                                    Indoor/Outdoor Games for Little Kids</li>
+                                <li class="wow fadeInLeft" data-wow-delay=".6s"><i class="far fa-check-circle"></i>
+                                    Professional & Qualified Teacher</li>
+                                <li class="wow fadeInLeft" data-wow-delay=".6s"><i class="far fa-check-circle"></i>
+                                    Indoor/Outdoor Games for Little Kids</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2">
+                    <div class="img_section">
+                        <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/about_img_shape_2.png" alt="#" class="about_img_7"
+                            data-parallax='{"x": 0, "y": -50, "rotateZ":0}'>
+                        <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/about_img_7.png" alt="#" class="about_img_6 img-fluid">
+                        <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/about_img_shape_1.png" alt="#" class="about_img_5"
+                            data-parallax='{"x": 0, "y": 50, "rotateZ":0}'>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="about_page_animation_1">
+            <div data-parallax='{"x": 2, "y": 70, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/icon_5.png" alt="#"></div>
+        </div>
+        <div class="about_page_animation_2">
+            <div data-parallax='{"x": 10, "y": 80, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/icon_6.png" alt="#"></div>
+        </div>
+        <div class="about_page_animation_3">
+            <div data-parallax='{"x": 30, "y": 60, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/icon_7.png" alt="#"></div>
+        </div>
+        <div class="about_page_animation_4">
+            <div data-parallax='{"x": 30, "y": -50, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/icon_8.png" alt="#"></div>
+        </div>
+    </section>
+    <!-- about section part end -->
+
+    <!-- fetures part here -->
+    <section class="fetures_part padding_bottom">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="section_tittle_style_02">
+                        <h2 class="title wow fadeInDown" data-wow-delay=".3s"> <span class="title_overlay_effect"> Educational Programs</span></h2>
+                        <p class="description wow fadeInDown" data-wow-delay=".3s">Kidzo mission is to provide affordable, 
+                        high-quality early education and childcare services for working families to ensure every child.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".2s">
+                    <div class="single_fetures_part">
+                        <div class="single_fetures_icon">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_img_01.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html"> Pre-School Sports</a></h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".2s">
+                    <div class="single_fetures_part">
+                        <div class="single_fetures_icon">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_img_01.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html"> Pre-School Sports</a></h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single_fetures_part lightblue_bg">
+                        <div class="single_fetures_icon">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_img_02.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Healthy Meals</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single_fetures_part pink_bg">
+                        <div class="single_fetures_icon">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/program_img_03.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Babysitting</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="feature_animation_1">
+            <div data-parallax='{"x": 2, "y": 120, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/program_list_shape_01.png" alt="#"></div>
+        </div>
+        <div class="feature_animation_2">
+            <div data-parallax='{"x": 10, "y": 100, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/program_list_shape_02.png" alt="#"></div>
+        </div>
+        <div class="feature_animation_3">
+            <div data-parallax='{"x": 30, "y": 110, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/program_list_shape_03.png" alt="#"></div>
+        </div>
+        <div class="feature_animation_4">
+            <div data-parallax='{"x": 5, "y": 105, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/program_list_shape_04.png" alt="#"></div>
+        </div>
+    </section>
+    <!-- fetures part end -->
