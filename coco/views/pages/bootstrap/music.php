20868879977f6357a4c0eb9135c4d4726685c00c
Hyerim Yeom <ringkle777@gmail.com>
2021-02-21 13:52:33 +0900

    #1559 music 음악플랫폼 레이아웃

--- coco/views/pages/bootstrap/music.php
+++ coco/views/pages/bootstrap/music.php
@@ -61,80 +61,67 @@
                 </div>
             </div>
             <div class="row">
-                <div class="row">
-                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".2s">
-                        <a class="single_service_part d-block" href="about.html">
-                            <div class="service_section_img_wrapper">
-                                <img src="img/services_1.png" alt="#" class="img-fluid">
-                            </div>
-                            <h5 class="service_title">Infant Care</h5>
-                        </a>
+                <div class="col-9">
+                    <div class="row">
+                        <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".2s">
+                            <a class="single_service_part d-block" href="about.html">
+                                <div class="service_section_img_wrapper">
+                                    <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/services_1.png" alt="#" class="img-fluid">
+                                </div>
+                                <h5 class="service_title">Infant Care</h5>
+                            </a>
+                        </div>
+                        <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".4s">
+                            <a class="single_service_part d-block bg_color_2" href="about.html">
+                                <div class="service_section_img_wrapper">
+                                    <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/services_2.png" alt="#" class="img-fluid">
+                                </div>
+                                <h5 class="service_title">Pre School</h5>
+                            </a>
+                        </div>
                     </div>
-                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".4s">
-                        <a class="single_service_part d-block bg_color_2" href="about.html">
-                            <div class="service_section_img_wrapper">
-                                <img src="img/services_2.png" alt="#" class="img-fluid">
-                            </div>
-                            <h5 class="service_title">Pre School</h5>
-                        </a>
+                    <div class="row">
+                        <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".2s">
+                            <a class="single_service_part d-block" href="about.html">
+                                <div class="service_section_img_wrapper">
+                                    <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/services_1.png" alt="#" class="img-fluid">
+                                </div>
+                                <h5 class="service_title">Infant Care</h5>
+                            </a>
+                        </div>
+                        <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".4s">
+                            <a class="single_service_part d-block bg_color_2" href="about.html">
+                                <div class="service_section_img_wrapper">
+                                    <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/services_2.png" alt="#" class="img-fluid">
+                                </div>
+                                <h5 class="service_title">Pre School</h5>
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".2s">
-                        <a class="single_service_part d-block" href="about.html">
-                            <div class="service_section_img_wrapper">
-                                <img src="img/services_1.png" alt="#" class="img-fluid">
-                            </div>
-                            <h5 class="service_title">Infant Care</h5>
-                        </a>
-                    </div>
-                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".4s">
-                        <a class="single_service_part d-block bg_color_2" href="about.html">
-                            <div class="service_section_img_wrapper">
-                                <img src="img/services_2.png" alt="#" class="img-fluid">
-                            </div>
-                            <h5 class="service_title">Pre School</h5>
-                        </a>
-                    </div>
-                </div>
-                <div id="music platform">
-                    <div class="col-sm-12 col-lg-12 wow fadeInDown" data-wow-delay=".4s">
-                        <a class="single_service_part d-block bg_color_2" href="about.html">
-                            <div class="service_section_img_wrapper">
-                                <img src="img/services_2.png" alt="#" class="img-fluid">
-                            </div>
-                            <h5 class="service_title">Pre School</h5>
-                        </a>
-                    </div>
+                <div class="col-3" id="music platform">
+                   <p>음악플랫폼</p>
                 </div>
             </div>
-            <!-- <a class="pc-button elementor-button button-link cu_btn mt-lg-4" href="#">
-                <div class="button-content-wrapper ">
-                    <span class="elementor-button-text">Learn More </span>
-                    <svg class="pc-dashes inner-dashed-border animated-dashes">
-                        <rect x="5px" y="5px" rx="22px" ry="22px" width="0" height="0"></rect>
-                    </svg>
-                </div>
-            </a> -->
         </div>
         <div class="services_shape_animation_1">
             <div data-parallax='{"x": 2, "y": 80, "rotateZ":0}'>
-                <img src="img/animation_shape/feature_shape_01.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/feature_shape_01.png" alt="#">
             </div>
         </div>
         <div class="services_shape_animation_2">
             <div data-parallax='{"x": 15, "y": 100, "rotateZ":0}'>
-                <img src="img/animation_shape/feature_shape_02.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/feature_shape_02.png" alt="#">
             </div>
         </div>
         <div class="services_shape_animation_3">
             <div data-parallax='{"x": 0, "y": 90, "rotateZ":0}'>
-                <img src="img/animation_shape/feature_shape_03.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/feature_shape_03.png" alt="#">
             </div>
         </div>
         <div class="services_shape_animation_4">
             <div data-parallax='{"x": 10, "y": 120, "rotateZ":0}'>
-                <img src="img/animation_shape/feature_shape_04.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/animation_shape/feature_shape_04.png" alt="#">
             </div>
         </div>
     </section>
