71c0eee161d6a40dd2946f9ec5ca1f0acfdab308
Hyerim Yeom <ringkle777@gmail.com>
2021-02-21 13:38:28 +0900

    #1559 music 레이아웃

--- coco/views/pages/bootstrap/music.php
+++ coco/views/pages/bootstrap/music.php
@@ -1,1 +1,141 @@
-music
+    <!-- breadcrumb part -->
+    <section class="breadcrumb_part parallax_bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 ">
+                    <div class="breadcrumb_iner">
+                        <h2>MUSIC</h2>
+                        <!-- <div class="breadcrumb_iner_link">
+                            <a href="index.html">Home</a>
+                            <span>|</span>
+                            <p>About us</p>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb_animation_4">
+            <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_5">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_7">
+            <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_10">
+            <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_12">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_13">
+            <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb part end -->
+
+    <!-- services part here 575부터 1->2 -->
+    <section class="services_part wave_shape_bg sec_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="section_tittle_style_02">
+                        <h2 class="title wow fadeInDown" data-wow-delay=".3s" > <span class="title_overlay_effect"> What We Offer</span></h2>
+                        <p class="description wow fadeInDown" data-wow-delay=".3s">Kidzo mission is to provide
+                            affordable, high-quality early education and
+                            childcare services for working families to ensure every child.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="row">
+                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".2s">
+                        <a class="single_service_part d-block" href="about.html">
+                            <div class="service_section_img_wrapper">
+                                <img src="img/services_1.png" alt="#" class="img-fluid">
+                            </div>
+                            <h5 class="service_title">Infant Care</h5>
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".4s">
+                        <a class="single_service_part d-block bg_color_2" href="about.html">
+                            <div class="service_section_img_wrapper">
+                                <img src="img/services_2.png" alt="#" class="img-fluid">
+                            </div>
+                            <h5 class="service_title">Pre School</h5>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".2s">
+                        <a class="single_service_part d-block" href="about.html">
+                            <div class="service_section_img_wrapper">
+                                <img src="img/services_1.png" alt="#" class="img-fluid">
+                            </div>
+                            <h5 class="service_title">Infant Care</h5>
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-lg-6 wow fadeInDown" data-wow-delay=".4s">
+                        <a class="single_service_part d-block bg_color_2" href="about.html">
+                            <div class="service_section_img_wrapper">
+                                <img src="img/services_2.png" alt="#" class="img-fluid">
+                            </div>
+                            <h5 class="service_title">Pre School</h5>
+                        </a>
+                    </div>
+                </div>
+                <div id="music platform">
+                    <div class="col-sm-12 col-lg-12 wow fadeInDown" data-wow-delay=".4s">
+                        <a class="single_service_part d-block bg_color_2" href="about.html">
+                            <div class="service_section_img_wrapper">
+                                <img src="img/services_2.png" alt="#" class="img-fluid">
+                            </div>
+                            <h5 class="service_title">Pre School</h5>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- <a class="pc-button elementor-button button-link cu_btn mt-lg-4" href="#">
+                <div class="button-content-wrapper ">
+                    <span class="elementor-button-text">Learn More </span>
+                    <svg class="pc-dashes inner-dashed-border animated-dashes">
+                        <rect x="5px" y="5px" rx="22px" ry="22px" width="0" height="0"></rect>
+                    </svg>
+                </div>
+            </a> -->
+        </div>
+        <div class="services_shape_animation_1">
+            <div data-parallax='{"x": 2, "y": 80, "rotateZ":0}'>
+                <img src="img/animation_shape/feature_shape_01.png" alt="#">
+            </div>
+        </div>
+        <div class="services_shape_animation_2">
+            <div data-parallax='{"x": 15, "y": 100, "rotateZ":0}'>
+                <img src="img/animation_shape/feature_shape_02.png" alt="#">
+            </div>
+        </div>
+        <div class="services_shape_animation_3">
+            <div data-parallax='{"x": 0, "y": 90, "rotateZ":0}'>
+                <img src="img/animation_shape/feature_shape_03.png" alt="#">
+            </div>
+        </div>
+        <div class="services_shape_animation_4">
+            <div data-parallax='{"x": 10, "y": 120, "rotateZ":0}'>
+                <img src="img/animation_shape/feature_shape_04.png" alt="#">
+            </div>
+        </div>
+    </section>
+    <!-- services part end -->
