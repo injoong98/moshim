add63de7d76ca86f87c2860846e5c4fff62c55e3
Hyerim Yeom <ringkle777@gmail.com>
2021-02-21 13:37:59 +0900

    #1558 youtube

--- coco/views/pages/bootstrap/youtube.php
+++ coco/views/pages/bootstrap/youtube.php
@@ -1,1 +1,165 @@
-aboutus
+    <!-- breadcrumb part -->
+    <section class="breadcrumb_part parallax_bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 ">
+                    <div class="breadcrumb_iner">
+                        <h2>YOUTUBE</h2>
+                        <!-- <div class="breadcrumb_iner_link">
+                            <a href="index.html">Home</a>
+                            <span>|</span>
+                            <p>About us</p>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb_animation_4">
+            <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_5">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_7">
+            <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_10">
+            <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_12">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_13">
+            <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb part end -->
+
+     <!-- get start part here -->
+     <section class="get_start_part section_padding section_bg">
+        <div class="container custom_container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-lg-7 wow fadeInUp" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                    <div class="video_section">
+                        <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/video_section_bg.png" alt="#" class="img-fluid">
+                        <a href="https://www.youtube.com/watch?v=oiHulAQmdqI" class="video_popup"><span class="polygon_shape"></span></a>
+                    </div>
+                </div>
+                <div class="col-lg-5 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="get_start_content mt-5 mt-lg-0">
+                        <h2 class="kid_title"> <span class="title_overlay_effect"> With Efficiency to More Opportunities </span></h2>
+                        <p>Why I say old chap that is spiffing bodge, blag pardon me mufty Oxford butty bubble and squeak wind 
+                        up, brown bread the full monty bloke ruddy cras tickety-boo squiffy.</p>
+                        <p>Why I say old chap that is spiffing bodge, blag pardon me mufty Oxford butty bubble and squeak wind 
+                        up, brown bread the full monty bloke ruddy cras tickety-boo squiffy.</p>
+                        <a href="#" class="get_start_btn">Get Started Now! <i class="ti-arrow-right"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="get_start_animation_1"><div data-parallax='{"x": 2, "y": 120, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/feature_1.png" alt="#"></div></div>
+        <div class="get_start_animation_3"><div data-parallax='{"x": 30, "y": 110, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/feature_3.png" alt="#"></div></div>
+        <div class="get_start_animation_6"><div data-parallax='{"x": 15, "y": 115, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/feature_6.png" alt="#"></div></div>
+        <div class="get_start_animation_9"><div data-parallax='{"x": 20, "y": 120, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/feature_13.png" alt="#"></div></div>
+        <div class="get_start_animation_11"><div data-parallax='{"x": 15, "y": 115, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/feature_12.png" alt="#"></div></div>
+        <div class="get_start_animation_12"><div data-parallax='{"x": 20, "y": 40, "rotateZ":0}'><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/feature_7.png" alt="#"></div></div>
+    </section>
+    <!-- get start part end -->
+
+    <section class="team_section single_page_team section_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="single_team_section">
+                        <a href="teacher_details.html" class="d-block teacher_profile_img">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/team/team_1.png" alt="#" class="img-fluid">
+                        </a>
+                        <a href="teacher_list.html" class="teacher_category">Instructor</a>
+                        <h4> <a href="teacher_details.html">Dan Bergeron</a></h4>
+                        <p>Mathematics</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="single_team_section">
+                        <a href="teacher_details.html" class="d-block teacher_profile_img">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/team/team_2.png" alt="#" class="img-fluid">
+                        </a>
+                        <a href="teacher_list.html" class="teacher_category">Instructor</a>
+                        <h4> <a href="teacher_details.html">Roshan Bhula</a></h4>
+                        <p>English Literature</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="single_team_section">
+                        <a href="teacher_details.html" class="d-block teacher_profile_img">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/team/team_3.png" alt="#" class="img-fluid">
+                        </a>
+                        <a href="teacher_list.html" class="teacher_category">Senior Teacher</a>
+                        <h4> <a href="teacher_details.html">Jessica Hamel</a></h4>
+                        <p>General Knowledge</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="single_team_section">
+                        <a href="teacher_details.html" class="d-block teacher_profile_img">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/team/team_member_8.jpg" alt="#" class="img-fluid">
+                        </a>
+                        <a href="teacher_list.html" class="teacher_category">Instructor</a>
+                        <h4> <a href="teacher_details.html">Kevin Burnes</a></h4>
+                        <p>Mathematics</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="single_team_section">
+                        <a href="teacher_details.html" class="d-block teacher_profile_img">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/team/team_member_9.jpg" alt="#" class="img-fluid">
+                        </a>
+                        <a href="teacher_list.html" class="teacher_category">Instructor</a>
+                        <h4> <a href="teacher_details.html">Adam Easton</a></h4>
+                        <p>English Literature</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <div class="single_team_section">
+                        <a href="teacher_details.html" class="d-block teacher_profile_img">
+                            <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/team/team_member_10.jpg" alt="#" class="img-fluid">
+                        </a>
+                        <a href="teacher_list.html" class="teacher_category">Senior Teacher</a>
+                        <h4> <a href="teacher_details.html">Aaron Sanders</a></h4>
+                        <p>General Knowledge</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="team_animation_1">
+            <div data-parallax="{&quot;x&quot;: 2, &quot;y&quot;: 120, &quot;rotateZ&quot;:0}" style="transform:translate3d(0.646px, 38.733px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0.646px, 38.733px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/story_animation_1.png" alt="#">
+            </div>
+        </div>
+        <div class="team_animation_2">
+            <div data-parallax="{&quot;x&quot;: 10, &quot;y&quot;: 100, &quot;rotateZ&quot;:0}" style="transform:translate3d(0.687px, 6.602px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0.687px, 6.602px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/story_animation_2.png" alt="#">
+            </div>
+        </div>
+        <div class="team_animation_3">
+            <div data-parallax="{&quot;x&quot;: 30, &quot;y&quot;: 110, &quot;rotateZ&quot;:0}" style="transform:translate3d(0.029px, 0.029px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0.029px, 0.029px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/story_animation_3.png" alt="#">
+            </div>
+        </div>
+        <div class="team_animation_4">
+            <div data-parallax="{&quot;x&quot;: 5, &quot;y&quot;: 105, &quot;rotateZ&quot;:0}" style="transform:translate3d(1.614px, 33.891px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(1.614px, 33.891px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/team_animation.png" alt="#"></div>
+        </div>
+        <div class="team_animation_5">
+            <div data-parallax="{&quot;x&quot;: 8, &quot;y&quot;: 110, &quot;rotateZ&quot;:0}" style="transform:translate3d(0.029px, 0.029px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -webkit-transform:translate3d(0.029px, 0.029px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); "><img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/story_animation_5.png" alt="#">
+            </div>
+        </div>
+    </section>
