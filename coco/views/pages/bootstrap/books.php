0b65e8153fdb3e7f5374c27ad66384d8071c8fb7
Hyerim Yeom <ringkle777@gmail.com>
2021-02-21 19:33:19 +0900

    #1561, #1560 동화책, 학습지 레이아웃

--- coco/views/pages/bootstrap/books.php
+++ coco/views/pages/bootstrap/books.php
@@ -1,50 +1,175 @@
-    <!-- breadcrumb part -->
-    <section class="breadcrumb_part parallax_bg">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6 ">
-                    <div class="breadcrumb_iner">
-                        <h2>동화책</h2>
-                        <!-- <div class="breadcrumb_iner_link">
-                            <a href="index.html">Home</a>
-                            <span>|</span>
-                            <p>About us</p>
-                        </div> -->
+    <body >
+        <!-- breadcrumb part -->
+        <section class="breadcrumb_part parallax_bg">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 ">
+                        <div class="breadcrumb_iner">
+                            <h2>동화책</h2>
+                            <!-- <div class="breadcrumb_iner_link">
+                                <a href="index.html">Home</a>
+                                <span>|</span>
+                                <p>About us</p>
+                            </div> -->
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="breadcrumb_animation_4">
-            <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+            <div class="breadcrumb_animation_4">
+                <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
+                    <img src="../assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+                </div>
             </div>
-        </div>
-        <div class="breadcrumb_animation_5">
-            <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+            <div class="breadcrumb_animation_5">
+                <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
+                    <img src="../assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+                </div>
             </div>
-        </div>
-        <div class="breadcrumb_animation_7">
-            <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+            <div class="breadcrumb_animation_7">
+                <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
+                    <img src="../assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+                </div>
             </div>
-        </div>
-        <div class="breadcrumb_animation_10">
-            <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+            <div class="breadcrumb_animation_10">
+                <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
+                    <img src="../assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+                </div>
             </div>
-        </div>
-        <div class="breadcrumb_animation_12">
-            <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+            <div class="breadcrumb_animation_12">
+                <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
+                    <img src="../assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+                </div>
             </div>
-        </div>
-        <div class="breadcrumb_animation_13">
-            <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/coco/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+            <div class="breadcrumb_animation_13">
+                <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
+                    <img src="../assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+                </div>
             </div>
-        </div>
-    </section>
-    <!-- breadcrumb part end -->
+        </section>
+        <!-- breadcrumb part end -->
 
-  
+        <!-- event list part here -->
+        <section class="event_list section_padding section_bg_1">
+            <div class="container custom_container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-7">
+                        <div class="section_tittle_style_02">
+                            <h2 class="title wow fadeInDown" data-wow-delay=".3s"> <span class="title_overlay_effect">Upcoming Events</span></h2>
+                            <p class="description wow fadeInDown" data-wow-delay=".3s">Kidzo mission is to provide affordable, high-quality 
+                            early education and childcare services for working families to ensure every child.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="single_event_list">
+                            <div class="event_list_img">
+                                <img src="img/event/list_event_1.png" alt="#" class="img-fluid">
+                            </div>
+                            <div class="event_list_content">
+                                <h5>12 Mar</h5>
+                                <h3> <a href="event_details.html"> Paint Happiness Day</a></h3>
+                                <p>The amazing way to olor sit adipisi sed do  way  consectetur way to olor sit adipisi</p>
+                                <ul>
+                                    <li><i class="fas fa-clock"></i>Time: <span>14:30 - 17:00</span></li>
+                                    <li><i class="fas fa-map-marker-alt"></i>Location: <span>Melbour</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="single_event_list">
+                            <div class="event_list_img">
+                                <img src="img/event/list_event_2.png" alt="#" class="img-fluid">
+                            </div>
+                            <div class="event_list_content">
+                                <h5>12 Mar</h5>
+                                <h3> <a href="event_details.html"> Paint Happiness Day</a></h3>
+                                <p>The amazing way to olor sit adipisi sed do  way  consectetur way to olor sit adipisi</p>
+                                <ul>
+                                    <li><i class="fas fa-clock"></i>Time: <span>14:30 - 17:00</span></li>
+                                    <li><i class="fas fa-map-marker-alt"></i>Location: <span>Melbour</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="single_event_list">
+                            <div class="event_list_img">
+                                <img src="img/event/list_event_3.png" alt="#" class="img-fluid">
+                            </div>
+                            <div class="event_list_content">
+                                <h5>12 Mar</h5>
+                                <h3> <a href="event_details.html"> Paint Happiness Day</a></h3>
+                                <p>The amazing way to olor sit adipisi sed do  way  consectetur way to olor sit adipisi</p>
+                                <ul>
+                                    <li><i class="fas fa-clock"></i>Time: <span>14:30 - 17:00</span></li>
+                                    <li><i class="fas fa-map-marker-alt"></i>Location: <span>Melbour</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="single_event_list">
+                            <div class="event_list_img">
+                                <img src="img/event/list_event_4.png" alt="#" class="img-fluid">
+                            </div>
+                            <div class="event_list_content">
+                                <h5>12 Mar</h5>
+                                <h3> <a href="event_details.html"> Paint Happiness Day</a></h3>
+                                <p>The amazing way to olor sit adipisi sed do  way  consectetur way to olor sit adipisi</p>
+                                <ul>
+                                    <li><i class="fas fa-clock"></i>Time: <span>14:30 - 17:00</span></li>
+                                    <li><i class="fas fa-map-marker-alt"></i>Location: <span>Melbour</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="single_event_list">
+                            <div class="event_list_img">
+                                <img src="img/event/list_event_5.png" alt="#" class="img-fluid">
+                            </div>
+                            <div class="event_list_content">
+                                <h5>12 Mar</h5>
+                                <h3> <a href="event_details.html"> Paint Happiness Day</a></h3>
+                                <p>The amazing way to olor sit adipisi sed do  way  consectetur way to olor sit adipisi</p>
+                                <ul>
+                                    <li><i class="fas fa-clock"></i>Time: <span>14:30 - 17:00</span></li>
+                                    <li><i class="fas fa-map-marker-alt"></i>Location: <span>Melbour</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="single_event_list">
+                            <div class="event_list_img">
+                                <img src="img/event/list_event_6.png" alt="#" class="img-fluid">
+                            </div>
+                            <div class="event_list_content">
+                                <h5>12 Mar</h5>
+                                <h3> <a href="event_details.html"> Paint Happiness Day</a></h3>
+                                <p>The amazing way to olor sit adipisi sed do  way  consectetur way to olor sit adipisi</p>
+                                <ul>
+                                    <li><i class="fas fa-clock"></i>Time: <span>14:30 - 17:00</span></li>
+                                    <li><i class="fas fa-map-marker-alt"></i>Location: <span>Melbour</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event_list_animation_1">
+                <div data-parallax='{"x": 2, "y": 70, "rotateZ":0}'><img src="img/icon/icon_5.png" alt="#"></div>
+            </div>
+            <div class="event_list_animation_3">
+                <div data-parallax='{"x": 30, "y": 60, "rotateZ":0}'><img src="img/icon/icon_7.png" alt="#"></div>
+            </div>
+            <div class="event_list_animation_4">
+                <div data-parallax='{"x": 30, "y": -50, "rotateZ":0}'><img src="img/icon/icon_8.png" alt="#" class="img-fluid"></div>
+            </div>
+        </section>
+    <!-- event list part end -->
+
+    </body>
+    
