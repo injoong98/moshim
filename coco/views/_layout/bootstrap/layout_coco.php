d21dfd58c9cd6cc0e182127c03c7437e50e44618
Hyerim Yeom <ringkle777@gmail.com>
2021-02-21 13:59:10 +0900

    #1545 ciboard 자바스크립트 변수 추가

--- coco/views/_layout/bootstrap/layout_coco.php
+++ coco/views/_layout/bootstrap/layout_coco.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>맑은물</title>
-    <link rel="icon" href="<?php echo trim(site_url(), '/'); ?>/coco/assets/malgeunmul/img/favicon.png" type="image/png">
+    <link rel="icon" href="<?php echo trim(site_url(), '/'); ?>/coco/assets/malgeunmul/img/favicon.ico">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo trim(site_url(), '/'); ?>/coco/assets/malgeunmul/css/bootstrap.min.css" />
     <!-- animate CSS -->
@@ -23,6 +23,25 @@
     <link rel="stylesheet" href="<?php echo trim(site_url(), '/'); ?>/coco/assets/malgeunmul/vendors/magnify_popup/magnific-popup.css" />
     <!-- style CSS -->
     <link rel="stylesheet" href="<?php echo trim(site_url(), '/'); ?>/coco/assets/malgeunmul/css/style.css" />
+
+	<script type="text/javascript">
+		// 자바스크립트에서 사용하는 전역변수 선언
+		var cb_url = "<?php echo trim(site_url(), '/'); ?>";
+		var cb_cookie_domain = "<?php echo config_item('cookie_domain'); ?>";
+		var cb_charset = "<?php echo config_item('charset'); ?>";
+		var cb_time_ymd = "<?php echo cdate('Y-m-d'); ?>";
+		var cb_time_ymdhis = "<?php echo cdate('Y-m-d H:i:s'); ?>";
+		var layout_skin_path = "<?php echo element('layout_skin_path', $layout); ?>";
+		var view_skin_path = "<?php echo element('view_skin_path', $layout); ?>";
+		var is_member = "<?php echo $this->member->is_member() ? '1' : ''; ?>";
+		var is_admin = "<?php echo $this->member->is_admin(); ?>";
+		var cb_admin_url = <?php echo $this->member->is_admin() === 'super' ? 'cb_url + "/' . config_item('uri_segment_admin') . '"' : '""'; ?>;
+		var cb_board = "<?php echo isset($view) ? element('board_key', $view) : ''; ?>";
+		var cb_board_url = <?php echo (isset($view) && element('board_key', $view)) ? 'cb_url + "/' . config_item('uri_segment_board') . '/' . element('board_key', $view) . '"' : '""'; ?>;
+		var cb_device_type = "<?php echo $this->cbconfig->get_device_type() === 'mobile' ? 'mobile' : 'desktop' ?>";
+		var cb_csrf_hash = "<?php echo $this->security->get_csrf_hash(); ?>";
+		var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
+	</script>
 </head>
 
 <body>
