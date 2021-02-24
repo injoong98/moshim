bdf5fb659a9ee33c96dc037308a3c3516a106747
Hyerim Yeom <ringkle777@gmail.com>
2021-02-24 18:29:16 +0900

    #1609 cd페이지

---
+++ momo/views/pages/bootstrap/cd.php
@@ -0,0 +1,235 @@
+   <!--Page Title-->
+   <section class="page-title centred" style="background-image: url(<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/cover_01.jpg);">
+       <div class="container">
+           <div class="content-box">
+               <h1>CD</h1>
+               <!-- <ul class="bread-crumb clearfix">
+                   <li><a href="index.html">Home</a></li>
+                   <li>About</li>
+               </ul> -->
+           </div>
+       </div>
+   </section>
+   <!--End Page Title-->
+
+   <!-- our-teachers -->
+   <section class="our-teachers style-three about-page sec-pad centred">
+        <div class="container">
+            <div class="sec-title">
+                <h5>동요 CD</h5>
+                <h1> 참어머님의 생애와 심정을 담은 효정의 노래(5 ~ 7세) </h1>
+            </div>
+            <!-- <div class="inner-content"> -->
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-1.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Kevin Spacey</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-2.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Catherine Jones</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-3.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">John Travolta</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-1.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Kevin Spacey</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-2.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Catherine Jones</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-3.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">John Travolta</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-1.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Kevin Spacey</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-2.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Catherine Jones</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-3.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">John Travolta</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-1.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Kevin Spacey</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-2.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">Catherine Jones</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+                        <div class="teachers-block-two wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <figure class="image-box"><a href="#"><img src="images/resource/teacher-3.jpg" alt=""></a></figure>
+                                <div class="lower-content">
+                                    <h3><a href="#">John Travolta</a></h3>
+                                    <!-- <span class="designation">Teacher</span>
+                                    <ul class="social-list">
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <!-- </div> -->
+        </div>
+    </section>
+    <!-- our-teachers end -->
+
