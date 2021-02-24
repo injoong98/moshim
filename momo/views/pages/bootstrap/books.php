52297334e06afd00295f77e0a71af011fd9af455
Hyerim Yeom <ringkle777@gmail.com>
2021-02-24 18:30:30 +0900

    #1607 동화책 화면 수정

--- momo/views/pages/bootstrap/books.php
+++ momo/views/pages/bootstrap/books.php
@@ -1,5 +1,5 @@
    <!--Page Title-->
-   <section class="page-title centred" style="background-image: url(<?php echo moshim_url('momo') ?>/assets/moshim/images/background/page-title.jpg);">
+   <section class="page-title centred" style="background-image: url(<?php echo moshim_url('momo') ?>/assets/moshim/images/book/cover_01.jpg);">
        <div class="container">
            <div class="content-box">
                <h1>동화책</h1>
@@ -15,8 +15,27 @@
    <!-- classes-section -->
    <section class="classes-section classes-page-section sec-pad">
        <div class="container">
+            <div class="sec-title">
+                <h5>참어머님 생애동화</h5>
+                <h1> 인류의 참어머님의 생애와 심정을 이해할 수 있는 귀한 보물과 같은 이야기 </h1>
+            </div>
            <div class="row">
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
+                   <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/01.jpg" alt=""></a></figure>
+                       <div class="lower-content">
+                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
+                           <h3><a href="#POPUPNEEDED">참어머님 생애동화 세트</a></h3>
+                           <div class="price">$480</div>
+                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <ul class="info-box">
+                               <li>Age: <span>2-4 Years</span></li>
+                               <li>Size: <span>12 Seats</span></li>
+                           </ul>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/01.jpg" alt=""></a></figure>
                        <div class="lower-content">
@@ -31,7 +50,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/02.jpg" alt=""></a></figure>
                        <div class="lower-content">
@@ -46,7 +65,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/03.jpg" alt=""></a></figure>
                        <div class="lower-content">
@@ -61,7 +80,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/04.jpg" alt=""></a></figure>
                        <div class="lower-content">
@@ -76,7 +95,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/05.jpg" alt=""></a></figure>
                        <div class="lower-content">
@@ -91,7 +110,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/06.jpg" alt=""></a></figure>
                        <div class="lower-content">
@@ -106,7 +125,7 @@
                        </div>
                    </div>
                </div>
-               <div class="col-lg-4 col-md-6 col-sm-12 block-column">
+               <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
                        <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/07.jpg" alt=""></a></figure>
                        <div class="lower-content">
