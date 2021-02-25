d830446d9cf05885a6467e123014da25efdc9737
pickyham <hammieprogrammer@gmail.com>
2021-02-25 16:47:14 +0900

    #1607 동화책 pdf 넣기

--- momo/views/pages/bootstrap/books.php
+++ momo/views/pages/bootstrap/books.php
@@ -15,127 +15,129 @@
    <!-- classes-section -->
    <section class="classes-section classes-page-section sec-pad">
        <div class="container">
-            <div class="sec-title">
-                <h5>참어머님 생애동화</h5>
-                <h1> 인류의 참어머님의 생애와 심정을 이해할 수 있는 귀한 보물과 같은 이야기 </h1>
-            </div>
+           <div class="sec-title">
+               <h5>참어머님 생애동화</h5>
+               <h1> 인류의 참어머님의 생애와 심정을 이해할 수 있는
+                   <br />귀한 보물과 같은 이야기
+               </h1>
+           </div>
            <div class="row">
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/01.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin();"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/01.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">참어머님 생애동화 세트</a></h3>
-                           <div class="price">$480</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin();"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin();">참어머님 생애동화 세트</a></h3>
+                           <div class="price">91,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>권수 : <span>총 7권 </span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/01.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(1);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/01.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">하늘이 준비하신 참어머님</a></h3>
-                           <div class="price">$480</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(1);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(1);">하늘이 준비하신 참어머님</a></h3>
+                           <div class="price">$13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>페이지 : <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/02.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(2);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/02.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">하늘의 사랑스러운 딸 참어머님</a></h3>
-                           <div class="price">$580</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(2);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(2);">하늘의 사랑스러운 딸 참어머님</a></h3>
+                           <div class="price">13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>페이지 : <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/03.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(3);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/03.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">순수하면서도 강인한 모범생 참어머님</a></h3>
-                           <div class="price">$550</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(3);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(3);">순수하면서도 강인한 모범생 참어머님</a></h3>
+                           <div class="price">13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>페이지 : <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/04.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(4);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/04.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">참아버님 참어머님 참부모님</a></h3>
-                           <div class="price">$500</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(4);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(4);">참아버님 참어머님 참부모님</a></h3>
+                           <div class="price">13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>페이지 : <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/05.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(5);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/05.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">참된 아내 참어머님</a></h3>
-                           <div class="price">$400</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(5);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(5);">참된 아내 참어머님</a></h3>
+                           <div class="price">13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>페이지 : <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/06.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(6);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/06.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">참사랑의 어머니 참어머님</a></h3>
-                           <div class="price">$350</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(6);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(6);">참사랑의 어머니 참어머님</a></h3>
+                           <div class="price">13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>페이지 : <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
                </div>
                <div class="col-lg-3 col-md-6 col-sm-12 block-column">
                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
-                       <figure class="image-box"><a href="#POPUPNEEDED"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/07.jpg" alt=""></a></figure>
+                       <figure class="image-box"><a onclick="javascript:openWin(7);"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/book/07.jpg" alt=""></a></figure>
                        <div class="lower-content">
-                           <div class="link-btn"><a href="#POPUPNEEDED"><i class="flaticon-next"></i></a></div>
-                           <h3><a href="#POPUPNEEDED">참부모 참어머님</a></h3>
-                           <div class="price">$350</div>
-                           <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div>
+                           <div class="link-btn"><a onclick="javascript:openWin(7);"><i class="flaticon-next"></i></a></div>
+                           <h3><a onclick="javascript:openWin(7);">참부모 참어머님</a></h3>
+                           <div class="price">13,000원</div>
+                           <!-- <div class="text">Lorem ipsum dolor sit amet, consectetur pisicelit sed do eiusmod tempor incidie labore magna aliqua.</div> -->
                            <ul class="info-box">
-                               <li>Age: <span>2-4 Years</span></li>
-                               <li>Size: <span>12 Seats</span></li>
+                               <li>연령 : <span>5 ~ 13세</span></li>
+                               <li>Size: <span>31 페이지</span></li>
                            </ul>
                        </div>
                    </div>
@@ -144,3 +146,9 @@
        </div>
    </section>
    <!-- classes-section end -->
+
+   <script language="javascript" type="text/javascript">
+       function openWin(num) {
+           window.open(`<?php echo moshim_url('momo') ?>/views/pages/bootstrap/pdf/[kr]0${num}_spread.pdf`, "동화책 자세히 보기", "width=800, height=700, toolbar=no, menubar=no, scrollbars=no, resizable=yes");
+       }
+   </script>
