68fe3f27da4ec73594e45804374e9ed783dfe688
Tune <cfdw011@gmail.com>
2021-02-25 20:44:57 +0900

    #1614 맑은물 뮤직 페이지 assets 링크 수정

--- coco/views/pages/bootstrap/music.php
+++ coco/views/pages/bootstrap/music.php
@@ -40,32 +40,32 @@
         </div>
         <div class="breadcrumb_animation_4">
             <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
             </div>
         </div>
         <div class="breadcrumb_animation_5">
             <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
             </div>
         </div>
         <div class="breadcrumb_animation_7">
             <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
             </div>
         </div>
         <div class="breadcrumb_animation_10">
             <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
             </div>
         </div>
         <div class="breadcrumb_animation_12">
             <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
             </div>
         </div>
         <div class="breadcrumb_animation_13">
             <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
-                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
             </div>
         </div>
     </section>
