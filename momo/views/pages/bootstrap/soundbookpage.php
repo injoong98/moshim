f2166bff6477dbd7f79106ec81edad535bca6ede
Hyerim Yeom <ringkle777@gmail.com>
2021-02-27 16:19:02 +0900

    #1608 사운드북 자세히 보기 화면

---
+++ momo/views/pages/bootstrap/soundbookpage.php
@@ -0,0 +1,14 @@
+
+    <img src="http://moshim.co.kr/assets/moshim/soundbook_track/img/<?php echo element('soundbookpage', $view) ?>.jpg"  width="100%" height="100%" style="width:800px">
+
+<div style="position: fixed; right: 0px; bottom:0px;">
+        <a href="">
+            <img src="<?php echo moshim_url('momo')?>/../assets/moshim/images/soundbook/note.png" alt="" width="10%">
+        </a>
+        <a href="">
+            <img src="<?php echo moshim_url('momo')?>/../assets/moshim/images/soundbook/audio.png" alt="" width="10%">
+        </a>
+    <!-- <img src="resources/img/serviceinfo/button_02.png"></img> -->
+</div>
+<!-- <img src="resources/img/serviceinfo/serviceinfo_07.png"></img> -->
+
