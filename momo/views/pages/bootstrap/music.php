11c65ce8acfa5addb0f5d14aeaf70aa451fa5eab
Hyerim Yeom <ringkle777@gmail.com>
2021-02-27 16:18:17 +0900

    #1559 음악부분 ( 추후 작업 )

---
+++ momo/views/pages/bootstrap/music.php
@@ -0,0 +1,322 @@
+<style>
+.audio_post{
+    margin-bottom: 28px;
+}
+.audioplayer.song{
+    display:flex;
+    justify-content:space-between;
+    height: 50px !important;
+}
+.mb{
+    margin-bottom:10px;
+}
+.buttons{
+    display:flex;
+    justify-content:space-between;
+    align-items: center;
+}
+.lyrics {
+    margin-right: 20px;
+}
+.music_sns_icon{
+    width: 66%;
+}
+</style>
+    <!-- breadcrumb part -->
+    <section class="breadcrumb_part parallax_bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 ">
+                    <div class="breadcrumb_iner">
+                        <h2>MUSIC</h2>
+                        <!-- <div class="breadcrumb_iner_link">
+                            <a href="index.html">Home</a>
+                            <span>|</span>
+                            <p>About us</p>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb_animation_4">
+            <div data-parallax='{"x": 30, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_4.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_5">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_5.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_7">
+            <div data-parallax='{"x": 100, "y": 250, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_15.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_10">
+            <div data-parallax='{"x": 15, "y": 150, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_10.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_12">
+            <div data-parallax='{"x": 20, "y": 150, "rotateZ":180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_20.png" alt="#">
+            </div>
+        </div>
+        <div class="breadcrumb_animation_13">
+            <div data-parallax='{"x": 10, "y": 250, "rotateZ": 180}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/icon/banner_icon/animated_banner_21.png" alt="#">
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb part end -->
+
+    <!-- services part here 575부터 1->2 -->
+    <section class="services_part wave_shape_bg sec_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="section_tittle_style_02">
+                        <h2 class="title wow fadeInDown" data-wow-delay=".3s" > <span class="title_overlay_effect"> What We Offer</span></h2>
+                        <p class="description wow fadeInDown" data-wow-delay=".3s">Kidzo mission is to provide
+                            affordable, high-quality early education and
+                            childcare services for working families to ensure every child.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                    효정의 마음
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=PGIQLgYRyIE&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                우리집 하트뿅
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=Cbqa9SIjs_U&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=2" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                하하하 하트
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=c5QNbnbwziY&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=3" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                    달리기 대장
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=MA-E_s2xx5s&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=4" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                    방방 Jump
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=dfoaL96E1OQ&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=5" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                    산에 산에 올라요
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=6aX6IoFL43U&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=6" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                좋아하는 것
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=yruy34odL9o&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=7" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                졸졸졸 내 동생
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=VnqPveocPRE&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=8" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                따라해봐요
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=31-ZrX9GALY&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=9" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audio_post">
+                        <div class="post_thumb mb">
+                            <div class="audioplayer song">
+                                <div class="text">
+                                    짝짝짝
+                                </div>
+                                <div class="buttons">
+                                    <div class="lyrics">
+                                        가사
+                                    </div>
+                                    <div class="youtube">
+                                        <a href="https://www.youtube.com/watch?v=LxgCxcq6uIM&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI&index=10" target="_blank">
+                                            <img src="/assets/images/youtube.png" width="50"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top:120px">
+                <div class="col-lg-12 d-flex justify-content-between">
+                    <a class="col-lg-2" target="_blank" href="https://www.youtube.com/watch?v=PGIQLgYRyIE&list=OLAK5uy_k0kLuO_sbA5pRPtKtWCvUKpcGxtHJSHeI">
+                        <img src="/assets/images/youtube.png" class="music_sns_icon"/>
+                    </a>
+                    <a class="col-lg-2" target="_blank" href="https://www.genie.co.kr/detail/albumInfo?axnm=81807819">
+                        <img src="/assets/images/genie.png" class="music_sns_icon"/>
+                    </a>
+                    <a class="col-lg-2" target="_blank" href="https://www.melon.com/album/detail.htm?albumId=10541078">
+                        <img src="/assets/images/melon.png" class="music_sns_icon"/>
+                    </a>
+                    <a class="col-lg-2" target="_blank" href="https://vibe.naver.com/album/5183052">
+                        <img src="/assets/images/vibe.png" class="music_sns_icon"/>
+                    </a>
+                    <a class="col-lg-2" target="_blank" href="https://music.apple.com/us/album/%EC%97%84%EB%A7%88%EB%9E%91-%EC%95%84%EB%B9%A0%EB%9E%91-%ED%9A%A8%EC%A0%95%EB%8F%99%EC%9A%94/1546079168?uo=4">
+                        <img src="/assets/images/applemusic_p.png" class="music_sns_icon"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="services_shape_animation_1">
+            <div data-parallax='{"x": 2, "y": 80, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/animation_shape/feature_shape_01.png" alt="#">
+            </div>
+        </div>
+        <div class="services_shape_animation_2">
+            <div data-parallax='{"x": 15, "y": 100, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/animation_shape/feature_shape_02.png" alt="#">
+            </div>
+        </div>
+        <div class="services_shape_animation_3">
+            <div data-parallax='{"x": 0, "y": 90, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/animation_shape/feature_shape_03.png" alt="#">
+            </div>
+        </div>
+        <div class="services_shape_animation_4">
+            <div data-parallax='{"x": 10, "y": 120, "rotateZ":0}'>
+                <img src="<?php echo trim(site_url(), '/') ?>/assets/malgeunmul/img/animation_shape/feature_shape_04.png" alt="#">
+            </div>
+        </div>
+    </section>
+    <!-- services part end -->
