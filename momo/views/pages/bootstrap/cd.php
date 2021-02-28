091f04aceef446ff3529ba96f116d7d3146ca77c
Hyerim Yeom <ringkle777@gmail.com>
2021-03-01 05:37:28 +0900

    #1609 동요 재생, 멈춤 자료조사중

--- momo/views/pages/bootstrap/cd.php
+++ momo/views/pages/bootstrap/cd.php
@@ -26,11 +26,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/07.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('1')" target="_blank">달래강 노래</a></h3>
+                                    <h3><a onclick="openWin_cd('1')" target="_blank">달래강 노래</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="<?php echo moshim_url('momo')?>/assets/moshim/soundbook_track/song/08.mp3"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('1')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-01 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('1')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -41,11 +41,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/09.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('2')" target="_blank">아름다운 이 세상</a></h3>
+                                    <h3><a onclick="openWin_cd('2')" target="_blank">아름다운 이 세상</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('2')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-02 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('2')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -56,11 +56,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/11.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('3')" target="_blank">하나님의 나의 부모님</a></h3>
+                                    <h3><a onclick="openWin_cd('3')" target="_blank">하나님의 나의 부모님</a></h3>
                                    <span class="designation"><br/></span>
                                    <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('3')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-03 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('3')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -71,11 +71,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/13.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('4')" target="_blank">수선화 사랑</a></h3>
+                                    <h3><a onclick="openWin_cd('4')" target="_blank">수선화 사랑</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('4')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-04 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('4')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -86,11 +86,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/15.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('5')" target="_blank">하늘 신부</a></h3>
+                                    <h3><a onclick="openWin_cd('5')" target="_blank">하늘 신부</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('5')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-05 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('5')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -101,11 +101,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/17.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('6')" target="_blank">산 넘고 바다 건너</a></h3>
+                                    <h3><a onclick="openWin_cd('6')" target="_blank">산 넘고 바다 건너</a></h3>
                                    <span class="designation"><br/></span>
                                    <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('6')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-06 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('6')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -118,11 +118,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/19.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('7')" target="_blank">효정의 어머니</a></h3>
+                                    <h3><a onclick="openWin_cd('7')" target="_blank">효정의 어머니</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('7')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-07 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('7')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -133,11 +133,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/21.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('8')" target="_blank">어머니 아들딸</a></h3>
+                                    <h3><a onclick="openWin_cd('8')" target="_blank">어머니 아들딸</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('8')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-08 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('8')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -148,11 +148,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/23.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('9')" target="_blank">어머니 손은 약손</a></h3>
+                                    <h3><a onclick="openWin_cd('9')" target="_blank">어머니 손은 약손</a></h3>
                                    <span class="designation"><br/></span>
                                    <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('9')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-09 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('9')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -163,11 +163,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/25.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('10')" target="_blank">해가 지고 달이 떠도</a></h3>
+                                    <h3><a onclick="openWin_cd('10')" target="_blank">해가 지고 달이 떠도</a></h3>
                                    <span class="designation"><br/></span>
                                    <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('10')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-10 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('10')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -178,11 +178,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/27.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('11')" target="_blank">아름다운 나라 천일국</a></h3>
+                                    <h3><a onclick="openWin_cd('11')" target="_blank">아름다운 나라 천일국</a></h3>
                                     <span class="designation"><br/></span>
                                     <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('11')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-11 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('11')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -193,11 +193,11 @@
                             <div class="inner-box">
                                 <figure class="image-box"><a href="#"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/cd/29.jpg" alt=""></a></figure>
                                 <div class="lower-content">
-                                    <h3><a onclick="openWin('12')" target="_blank">새로운 세상 우리 함께해요</a></h3>
+                                    <h3><a onclick="openWin_cd('12')" target="_blank">새로운 세상 우리 함께해요</a></h3>
                                    <span class="designation"><br/></span>
                                    <ul class="social-list">
-                                        <li><a href="#"><i class="fas fa-play"></i></a></li>
-                                        <li><a onclick="openWin('12')" target="_blank"><i class="far fa-file-alt"></i></a></li>
+                                        <li><a href="#" class="play_button"><i class="track-12 fas fa-play"></i></a></li>
+                                        <li><a onclick="openWin_cd('12')" target="_blank"><i class="far fa-file-alt"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -208,8 +208,10 @@
         </div>
     </section>
     <!-- our-teachers end -->
-    <script language="javascript" type="text/javascript">
-       function openWin(num) {
-           window.open(`http://moshim.co.kr/pages/cd_detail/${num}`,'가사&해설', 'width=740, height=500, scrollbars=no');
-       }
-   </script>
+  <script>
+      //cd 가사 해설 창 
+        function openWin_cd(num) {
+            window.open(`http://moshim.co.kr/pages/cd_detail/${num}`,'가사&해설', 'width=740, height=500, scrollbars=no');
+            return false
+        }
+  </script>
