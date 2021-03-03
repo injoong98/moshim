d19f931025b1d75846f12903ea644f292aef03e3
Tune <cfdw011@gmail.com>
2021-03-03 13:31:48 +0900

    #1628 굿즈페이지 디자인 적용

--- coco/views/pages/bootstrap/goods.php
+++ coco/views/pages/bootstrap/goods.php
@@ -61,6 +61,140 @@
         </div>
     </section>
     <!-- breadcrumb part end -->
+    <!-- fetures part here -->
+    <section class="fetures_part section_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="section_tittle_style_02">
+                        <h2 class="title wow fadeInDown" data-wow-delay=".3s"> <span class="title_overlay_effect"> Educational Programs</span></h2>
+                        <p class="description wow fadeInDown" data-wow-delay=".3s">Kidzo mission is to provide affordable, 
+                        high-quality early education and childcare services for working families to ensure every child.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".2s">
+                    <div class="single_fetures_part">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_01.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html"> Pre-School</a></h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single_fetures_part lightblue_bg">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_02.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Healthy Meals</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single_fetures_part pink_bg">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_03.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Babysitting</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single_fetures_part pink_bg">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_03.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Babysitting</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-between mt-5">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".2s">
+                    <div class="single_fetures_part">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_01.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html"> Pre-School</a></h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single_fetures_part lightblue_bg">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_02.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Healthy Meals</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single_fetures_part pink_bg">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_03.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Babysitting</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single_fetures_part pink_bg">
+                        <div class="single_fetures_icon">
+                            <img src="/assets/malgeunmul/img/program_img/program_img_03.png" alt="#" class="img-fluid">
+                        </div>
+                        <h4> <a href="about.html">Babysitting</a> </h4>
+                        <p>The amazing way to olor sit adipisicing elit, sed do consectetur </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="feature_animation_1">
+            <div data-parallax='{"x": 2, "y": 120, "rotateZ":0}'><img src="/assets/malgeunmul/img/animation_shape/program_list_shape_01.png" alt="#"></div>
+        </div>
+        <div class="feature_animation_2">
+            <div data-parallax='{"x": 10, "y": 100, "rotateZ":0}'><img src="/assets/malgeunmul/img/animation_shape/program_list_shape_02.png" alt="#"></div>
+        </div>
+        <div class="feature_animation_3">
+            <div data-parallax='{"x": 30, "y": 110, "rotateZ":0}'><img src="/assets/malgeunmul/img/animation_shape/program_list_shape_03.png" alt="#"></div>
+        </div>
+    </section>
+    <!-- fetures part end -->
+    <!-- event section part here -->
+    <section class="event_section section_padding event_details">
+        <div class="container custom_container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-sm-12">
+                    <div class="event_details_wrapper">
+                        <div class="event_details_thumb">
+                            <!-- <img src="/assets/malgeunmul/img/event/event_details_02.jpg" alt="#" class="img-fluid"> -->
+                            <iframe
+                                src="https://www.youtube.com/embed/PGIQLgYRyIE"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                allowfullscreen
+                                style="width:100%; aspect-ratio: 16 / 9;"
+                            ></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>   
+        <div class="about_page_animation_1">
+            <div data-parallax='{"x": 2, "y": 70, "rotateZ":0}'><img src="/assets/malgeunmul/img/icon/icon_7.png" alt="#"></div>
+        </div>
+        <div class="about_page_animation_2">
+            <div data-parallax='{"x": 10, "y": 80, "rotateZ":0}'><img src="/assets/malgeunmul/img/icon/event_6.png" alt="#"></div>
+        </div>
+        <div class="about_page_animation_3">
+            <div data-parallax='{"x": 30, "y": 60, "rotateZ":0}'><img src="/assets/malgeunmul/img/icon/banner_two_2.png" alt="#"></div>
+        </div>
+        <div class="about_page_animation_4">
+            <div data-parallax='{"x": 30, "y": -50, "rotateZ":0}'><img src="/assets/malgeunmul/img/icon/team_animation.png" alt="#" class="img-fluid"></div>
+        </div>
+    </section>
+    <!-- event section part end -->
     <!-- team part here -->
     <section class="team_section single_page_team section_padding">
         <div class="container">
