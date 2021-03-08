e646d173009e0c3bb8387e700c4feda426499dc7
injoong98 <68066289+injoong98@users.noreply.github.com>
2021-03-08 12:01:12 +0900

    #1631 로고 작은사이즈로 변경 + 소개글 배경색상 변경

--- coco/views/pages/bootstrap/workbooks.php
+++ coco/views/pages/bootstrap/workbooks.php
@@ -145,7 +145,7 @@
                             <div class="row align-items-center justify-content-center">
                                 <div class="col-lg-4 wow fadeInUp" data-wow-delay=".3s">
                                     <div class="program_details_thumb profile_img pr-lg-3">
-                                        <img src="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/main/main_withi_logo.jpg" alt="#" class="img-fluid">
+                                        <img src="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/workbook/withi_logo_small.jpg" alt="#" class="img-fluid">
                                         <!-- <img src="img/teacher_profile_shape.png" alt="#" class="img-fluid teacher_profile_shape"> -->
                                     </div>
                                 </div>
