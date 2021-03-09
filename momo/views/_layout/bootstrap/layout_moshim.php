2e6493e8f4c3932bd2747c2a4bedb5e85416a15b
pickyham <hammieprogrammer@gmail.com>
2021-03-09 14:18:21 +0900

    #1655 상단바 로고 크게 유지/ 상단바 메뉴 위치 조정

--- momo/views/_layout/bootstrap/layout_moshim.php
+++ momo/views/_layout/bootstrap/layout_moshim.php
@@ -248,7 +248,99 @@
 			</div>
 		</div>
 
-
+		<!--Sticky Header-->
+		<div class="sticky-header">
+			<div class="container clearfix">
+				<!-- <figure class="logo-box">
+				<a href="/"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/logo_1.jpg" alt="" style="height:46px"></a></figure> -->
+				<figure style="position: relative; display: inline-block;"><a href="/"><img src="<?php echo moshim_url('momo') ?>/assets/moshim/images/logo_1.jpg" alt="" style="height:125px"></a></figure>
+				<div class="menu-area">
+					<nav class="main-menu navbar-expand-lg">
+						<div class="navbar-header">
+							<!-- Toggle Button -->
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="navbar-collapse collapse clearfix">
+							<ul class="navigation clearfix">
+								<!-- <li class="current dropdown"><a href="/">Home</a> -->
+								<!-- <ul>
+										<li><a href="index.html">Home Page 01</a></li>
+										<li><a href="index-2.html">Home Page 02</a></li>
+										<li><a href="index-3.html">Home Page 03</a></li>
+										<li class="dropdown"><a href="#">Header Styles</a>
+											<ul>
+												<li><a href="index.html">Header Style 01</a></li>
+												<li><a href="index-2.html">Header Style 02</a></li>
+												<li><a href="index-3.html">Header Style 03</a></li>
+											</ul>
+										</li>
+									</ul> -->
+								</li>
+								<li class="dropdown <?php echo (element('menu_name', $layout) == 'main') ? 'current' : ''; ?>"><a href="http://moshim.co.kr/#intro"><?php echo (element('menu_name', $layout) == 'main') ? '' : '메인/'; ?>소개</a>
+									<!-- <li class="dropdown"><a href="/pages/vision">비전</a> -->
+									<!-- 	<ul>
+										<li><a href="about.html">About Us</a></li>
+										<li><a href="faq.html">Faq Page</a></li>
+										<li><a href="testimonial.html">Testimonials</a></li>
+										<li><a href="error.html">Error Page</a></li>
+									</ul> -->
+								</li>
+								<!-- <li class="dropdown"><a href="/pages/youtube">유튜브</a>
+										<ul>
+										<li><a href="class.html">Our Classes</a></li>
+										<li><a href="class-details.html">Class Details</a></li>
+									</ul>
+								</li> -->
+								<li class="dropdown
+								<?php echo (element('menu_name', $layout) === 'books') ? 'current' : ''; ?>
+								"><a href="/pages/books">동화책</a>
+									<!-- <ul>
+										<li><a href="gallery.html">Our Gallery</a></li>
+										<li><a href="teachers.html">Our Teachers</a></li>
+										<li><a href="pricing.html">Pricing Table</a></li>
+										<li><a href="event.html">Our Events</a></li>
+										<li><a href="event-details.html">Event Details</a></li>
+										<li><a href="shop.html">Shop Page</a></li>
+										<li><a href="shop-details.html">Shop Details</a></li>
+										<li><a href="cart.html">Cart Page</a></li>
+										<li><a href="checkout.html">Checkout Page</a></li>
+									</ul> -->
+								</li>
+								<li class="
+								<?php echo (element('menu_name', $layout) === 'soundbooks') ? 'current' : ''; ?>
+								dropdown"><a href="/pages/soundbooks">사운드북</a>
+									<!-- <ul>
+										<li><a href="gallery.html">Our Gallery</a></li>
+										<li><a href="teachers.html">Our Teachers</a></li>
+										<li><a href="pricing.html">Pricing Table</a></li>
+										<li><a href="event.html">Our Events</a></li>
+										<li><a href="event-details.html">Event Details</a></li>
+										<li><a href="shop.html">Shop Page</a></li>
+										<li><a href="shop-details.html">Shop Details</a></li>
+										<li><a href="cart.html">Cart Page</a></li>
+										<li><a href="checkout.html">Checkout Page</a></li>
+									</ul> -->
+								</li>
+								<li class="
+								<?php echo (element('menu_name', $layout) === 'cd') ? 'current' : ''; ?>
+								dropdown"><a href="/pages/cd">CD</a>
+									<!-- <ul>
+										<li><a href="blog.html">Blog Page</a></li>
+										<li><a href="blog-details.html">Blog Details</a></li>
+									</ul> -->
+								</li>
+								<!-- <li><a href="/pages/shop">SHOP</a></li> -->
+								<!-- <li><a href="/pages/login">로그인</a></li> -->
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div><!-- sticky-header end -->
 	</header>
 	<!-- End Main Header -->
 
