fedf02c769fee337ee4b56a2a27ae665421cc098
injoong98 <68066289+injoong98@users.noreply.github.com>
2021-03-30 23:10:16 +0900

    #1702 파비콘 적용

--- coco/views/_layout/bootstrap/layout_coco.php
+++ coco/views/_layout/bootstrap/layout_coco.php
@@ -6,9 +6,31 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>맑은물</title>
-    <link rel="icon" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/favicon.ico">
-    <link rel="shortcut icon" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/favicon.ico" />
-    <link rel="apple-touch-icon" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/favicon.ico" />
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/icon/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    
+    <!-- <link rel="shortcut icon" type="image/png" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/android-icon-144x144.png" /> -->
+    <link rel="icon" type="image/x-icon" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/favicon.ico">
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/favicon.ico" /> -->
+    <!-- <link rel="apple-touch-icon" type="image/x-icon" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/img/favicon.ico" /> -->
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo trim(site_url(), '/'); ?>/assets/malgeunmul/css/bootstrap.min.css" />
     <!-- animate CSS -->
